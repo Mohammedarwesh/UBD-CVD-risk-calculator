af8928659a921b1990409c9c2916909684aa0275
Mohammed Darwesh <64235988+Mohammedarwesh@users.noreply.github.com>
2023-01-19 21:17:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# UBD CVD risk calculator
+
+### Soft and Tools Requirements
+
+1. [Github Accound](https://github.com/Mohammedarwesh)
+2. [GitCLI]
+3. [Herokuaccount](https://dashboard.heroku.com/apps)
---
+++ UBD_CVD.py
@@ -0,0 +1,37 @@
+import numpy as np
+from flask import Flask, request,app,url_for,jsonify, render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+app = Flask(__name__)
+## load
+model = pickle.load(open('validated_model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    data = [float(x) for x in request.form.values()]
+    scaled_data = scaler.transform(np.array(data).reshape(1,-1))
+    print(scaled_data)
+    output = model.predict(scaled_data)
+    risk = output
+    msg = "Low CVD risk" if risk == 0 else "High CVD risk"
+    probapility = model.predict_proba(scaled_data)
+    probapility_round = round(probapility[0,1]*100)
+
+
+    return render_template('index.html', 
+                           prediction_text=f'You are of {msg} and Your CVD risk is about {probapility_round}%')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
